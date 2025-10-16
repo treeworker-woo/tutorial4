40c67868afead156b2a921a787604c3c622597ef
treeworker-woo <2630508215@qq.com>
2025-10-16 16:32:28 +0800

    Add files via upload

---
+++ src/main/java/org/example/Calc.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calc {
+
+    public int add(int x, int y)
+    {
+        return x+y;
+    }
+
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
